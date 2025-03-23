6f14c8398cc088390e1653ae9d8610ab77bc3740
xkYZI <1120985085@qq.com>
2025-03-23 22:47:56 +0800

    Site updated: YYYY-03-2025年3月23日 22:47:53

---
+++ js/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+  container: document.getElementById("aplayer"),
+  fixed: true,
+  autoplay: false,
+  theme: "#FADFA3",
+  audio: [
+    {
+      name: "瞬息间是重逢 (Then I see you again)",
+      artist: "鹿喑kana / kinsen",
+      url: "https://music.163.com/song/media/outer/url?id=1952188964",
+      cover:
+        "https://p2.music.126.net/RP9cW2hT6NkKLEuP47aKng==/109951167466259419.jpg?param=130y130",
+    },
+  ],
+});
