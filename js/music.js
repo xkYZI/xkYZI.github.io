09dc6b17d124a9263396b5b69d3f4b1fd9b8a6db
xkYZI <1120985085@qq.com>
2025-08-14 17:51:16 +0800

    Site updated: YYYY-08-2025年8月14日 17:51:15

--- js/music.js
+++ js/music.js
@@ -5,11 +5,11 @@ const ap = new APlayer({
   theme: "#FADFA3",
   audio: [
     {
-      name: "瞬息间是重逢 (Then I see you again)",
-      artist: "鹿喑kana / kinsen",
-      url: "https://music.163.com/song/media/outer/url?id=1952188964",
+      name: "垂泪之时 Lacrimosa",
+      artist: "HOYO-MiX",
+      url: "https://music.163.com/song/media/outer/url?id=2156623376",
       cover:
-        "https://p2.music.126.net/RP9cW2hT6NkKLEuP47aKng==/109951167466259419.jpg?param=130y130",
+        "http://p1.music.126.net/A59b7v8gBBxYI9pn8mO-Hw==/109951169594127870.jpg?param=130y130",
     },
   ],
 });
