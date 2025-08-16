cc6eeff5a664c3b3994032099aa249dacb3b87ff
xkYZI <1120985085@qq.com>
2025-08-16 20:06:09 +0800

    Site updated: YYYY-08-2025年8月16日 20:06:07

--- js/music.js
+++ js/music.js
@@ -11,5 +11,26 @@ const ap = new APlayer({
       cover:
         "http://p1.music.126.net/A59b7v8gBBxYI9pn8mO-Hw==/109951169594127870.jpg?param=130y130",
     },
+    {
+      name: "灵光的回响 Resonance of Khvarena",
+      artist: "HOYO-MiX",
+      url: "https://music.163.com/song/media/outer/url?id=2041862692",
+      cover:
+        "https://p1.music.126.net/f9Hbu5v27bGmg5ALM8iN9Q==/109951168565989598.jpg?param=130y130",
+    },
+    {
+      name: "空虚的愿景 Oracle of the Void",
+      artist: "HOYO-MiX",
+      url: "https://music.163.com/song/media/outer/url?id=2041862711",
+      cover:
+        "https://p1.music.126.net/f9Hbu5v27bGmg5ALM8iN9Q==/109951168565989598.jpg?param=130y130",
+    },
+    {
+      name: "银白的希望 Fragile Fantasy",
+      artist: "HOYO-MiX",
+      url: "https://music.163.com/song/media/outer/url?id=1833806442",
+      cover:
+        "https://p2.music.126.net/_U__1_DT6eLbvObNtK3apg==/109951165849462124.jpg?param=130y130",
+    },
   ],
 });
